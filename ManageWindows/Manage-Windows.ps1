37dd336c27a303c0a1fed28b66039c709aac35e3
Chobot <motofooko@gmail.com>
2022-09-11 20:43:57 +0200

    added windows management
    no documentation for now

---
+++ ManageWindows/Manage-Windows.ps1
@@ -0,0 +1,30 @@
+﻿. $PSScriptRoot'\Set-Window-ex.ps1'
+
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+$proc_ids = (Get-Process -Name d2r).Id | Sort-Object Id
+
+Write-Host $proc_ids
+
+# start position x,y ===========
+$x = 2560
+$y = -20
+#===============================
+
+$cnt = 0
+
+foreach ($id in $proc_ids){
+#creates one column of windows, after 7 instances it will start from top somewhere else at new x,y
+if ($cnt -eq 7){$x = 1200;$y=-20;}
+Set-Window -ProcessId $id -X $x -Y $y -Width 860 -Height 540
+Write-Host "Setting PID" $id "to position x: " $x " y: " $y
+# position adjustment per each instance x,y
+$x += 50
+$y += 38 
+#==========================================
+
+$cnt += 1 
+
+}
+
+#read-host "Press ENTER to continue..."
