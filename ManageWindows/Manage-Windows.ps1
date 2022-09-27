984442f057d1695f526d0d41a3d2fa39496061cf
Chobot <motofooko@gmail.com>
2022-09-27 03:38:14 +0200

    manage windows
    proc ids order

--- ManageWindows/Manage-Windows.ps1
+++ ManageWindows/Manage-Windows.ps1
@@ -2,7 +2,7 @@
 
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
-$proc_ids = (Get-Process -Name d2r).Id
+$proc_ids = (Get-Process -Name d2r | sort-object -property StartTime).Id
 
 Write-Host $proc_ids
 
